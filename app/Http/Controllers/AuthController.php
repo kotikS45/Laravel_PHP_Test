958f83c49e00f76a3c5d69b0f50ec978755240f0
kotikS45 <138294706+kotikS45@users.noreply.github.com>
2025-01-29 21:26:29 +0200

    Add login method.

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -15,6 +15,11 @@ class AuthController extends Controller
         return view('auth.register');
     }
 
+    public function showLoginForm()
+    {
+        return view('auth.login');
+    }
+
     /**
      * @OA\Post(
      *   path="/register",
@@ -76,5 +81,67 @@ class AuthController extends Controller
 
         $token = auth()->login($user);
         return response()->json(['token' => $token], Response::HTTP_CREATED);
+    }
+
+    /**
+     * @OA\Post(
+     *   path="/login",
+     *   tags={"Auth"},
+     *   summary="Login",
+     *   operationId="login",
+     *   @OA\RequestBody(
+     *     required=true,
+     *     description="User login data",
+     *     @OA\MediaType(
+     *       mediaType="application/json",
+     *       @OA\Schema(
+     *         required={"email", "password"},
+     *         @OA\Property(property="email", type="string"),
+     *         @OA\Property(property="password", type="string"),
+     *       )
+     *     )
+     *   ),
+     *   @OA\Response(
+     *     response=200,
+     *     description="Success",
+     *     @OA\MediaType(
+     *       mediaType="application/json"
+     *     )
+     *   ),
+     *   @OA\Response(
+     *     response=401,
+     *     description="Unauthenticated"
+     *   ),
+     *   @OA\Response(
+     *     response=400,
+     *     description="Bad Request"
+     *   ),
+     *   @OA\Response(
+     *     response=404,
+     *     description="Not Found"
+     *   ),
+     *   @OA\Response(
+     *     response=403,
+     *     description="Forbidden"
+     *   )
+     * )
+     */
+    public function login(Request $request) {
+        $validation = Validator::make($request->all(),[
+            'email'=> 'required|email',
+            'password'=> 'required|string|min:6'
+        ], [
+            'email.required' => 'Email is required',
+            'email.email' => 'Email is invalid',
+            'password.required' => 'Password cannot be empty',
+            'password.min' => 'Password must be at least 6 characters',
+        ]);
+        if($validation->fails()) {
+            return response()->json($validation->errors(), Response::HTTP_UNPROCESSABLE_ENTITY);
+        }
+        if(!$token = auth()->attempt($validation->validated())) {
+            return response()->json(['error'=>'Incorrect data!'], Response::HTTP_UNAUTHORIZED);
+        }
+        return response()->json(['token'=>$token], Response::HTTP_OK);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -9,3 +9,5 @@ Route::get('/', function () {
 
 Route::get("/register", [AuthController::class, 'showRegisterForm'])->name('register.form');
 Route::post('/register', [AuthController::class, 'register']);
+Route::get("/login", [AuthController::class, 'showLoginForm'])->name('login.form');
+Route::post('/login', [AuthController::class, 'login']);
